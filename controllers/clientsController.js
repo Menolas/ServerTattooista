25539a5c96d337a8f6623f2283edc5f096f193ea
Elena Christensen <olenachristensen777@gmail.com>
2023-05-28 21:28:05 +0300

    client edit api

--- controllers/clientsController.js
+++ controllers/clientsController.js
@@ -23,51 +23,76 @@ class clientsController {
     res.send(res.client);
   }
 
-  // async addClient(req, res) {
-  //   const client = new Client({
-  //     fullName: req.body.name,
-  //     avatar: req.protocol + '://' + req.get('host') + '/uploads/' + req.file.filename,
-  //     contacts: {
-  //       email: req.body.email,
-  //       insta: req.body.insta,
-  //       phone: req.body.phone,
-  //       whatsapp: req.body.whatsapp,
-  //       messenger: req.body.messenger
-  //     },
-  //     gallery: req.body.gallery
-  //   });
-  //
-  //   try {
-  //     let newClient = await client.save();
-  //     let result = newClient._id
-  //     res.status(201).json({result});
-  //   } catch (err) {
-  //     const result = 1
-  //     res.status(400).json({ message: err.message, result: result })
-  //   }
-  // }
+  async editClient(req, res) {
+    if (!req.body) {
+      return res.status(400).send({
+        message: "Data to update can not be empty!"
+      });
+    }
+
+    res.client.fullName = req.body.name,
+    res.client.contacts.email = req.body.email
+    res.client.contacts.insta = req.body.insta
+    res.client.contacts.phone = req.body.phone
+    res.client.contacts.whatsapp = req.body.whatsapp
+    res.client.contacts.messenger = req.body.messenger
+
+    try {
+      const updatedClient = await res.client.save();
+      let result = updatedClient._id
+      if (req.files.avatar) {
+        const file = req.files.avatar
+        const newFileName = encodeURI(Date.now() + '_' + file.name)
+        await file.mv(`./uploads/${result}/${newFileName}`, err => {
+          res.client.avatar = newFileName
+          res.client.save();
+        });
+      }
+
+      res.json(result);
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+  }
+
+  async updateClientGallery(req, res) {
+    if (!req.files) {
+      return res.status(400).send({
+        message: "Data to update can not be empty!"
+      });
+    }
+
+    const files = req.files
+    console.log(files)
+    const oldData = [...res.client.gallery]
+    //res.client.gallery = [...oldData, ...files]
+
+    try {
+      const updatedClient = await res.client.save();
+      res.json(updatedClient);
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+  }
 
   async addClient(req, res) {
     const client = new Client({
       fullName: req.body.name,
-      //avatar: req.protocol + '://' + req.get('host') + '/uploads/' + req.file.filename,
       contacts: {
         email: req.body.email,
         insta: req.body.insta,
         phone: req.body.phone,
         whatsapp: req.body.whatsapp,
         messenger: req.body.messenger
-      },
-      gallery: req.body.gallery
+      }
     });
 
     try {
       let newClient = await client.save();
       let result = newClient._id
-      console.log(req)
 
       const file = req.files.avatar
-      //if (!file) return res.json({ error: 'Incorrect input name'})
+      if (!file) return res.json({ error: 'Incorrect input name'})
       const newFileName = encodeURI(Date.now() + '_' + file.name)
       await file.mv(`./uploads/${result}/${newFileName}`, err => {
         client.avatar = newFileName
@@ -93,26 +118,6 @@ class clientsController {
     } catch (err) {
       const result = 1
       res.status(400).json({ message: err.message, result: result })
-    }
-  }
-
-  async updateClientAvatar(req, res) {
-    console.log("!!!!!!!!!!+++++++++++++++++++")
-    if (!req.body) {
-      return res.status(400).send({
-        message: "Data to update can not be empty!"
-      });
-    }
-
-    console.log(res.client + "!!!!!!!!!!!")
-    //res.client.avatar = req.body.avatar;
-
-    try {
-      console.log(req.body + "try!!!!!!!!!!")
-      //const updatedClient = await res.client.save();
-      //res.json(updatedClient);
-    } catch (err) {
-      res.status(400).json({ message: err.message });
     }
   }
 
